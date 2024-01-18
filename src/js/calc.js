718934a7a2abff368a656eed044a55114d2b0cce
Thanasis09 <57492658+Thanasis09@users.noreply.github.com>
2024-01-18 18:02:52 +0200

    more ui things

--- src/js/calc.js
+++ src/js/calc.js
@@ -5,4 +5,22 @@ document.addEventListener('DOMContentLoaded', function () {
     darkModeToggle.addEventListener('click', function () {
         body.classList.toggle('dark');
     });
+
+    // Calculator
+
+    // 2nd button
+
+    const secondElements = document.getElementsByClassName('disappear-2nd');
+    const thirdElements = document.getElementsByClassName('disappear-3rd');
+    const secondBtn = document.getElementById('secondBtn');
+
+    secondBtn.addEventListener('click', function() {
+        for (var i = 0; i < secondElements.length; i++) {
+            secondElements[i].classList.toggle('hidden');
+        }
+        for (var i = 0; i < thirdElements.length; i++) {
+            thirdElements[i].classList.toggle('hidden');
+        }
+    });
+
 });
